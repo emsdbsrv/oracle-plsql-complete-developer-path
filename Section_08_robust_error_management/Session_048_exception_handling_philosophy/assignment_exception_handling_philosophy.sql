8ce7644f7f4a919fb86b31155fff663280224612
avi <contact@emultiskills.com>
2025-11-08 19:16:04 +0530

    Auto-sync: 2025-11-08 19:16:04

---
+++ Section_08_robust_error_management/Session_048_exception_handling_philosophy/assignment_exception_handling_philosophy.sql
@@ -0,0 +1,4 @@
+SET SERVEROUTPUT ON;
+-- 10 tasks with commented solutions: predefined, OTHERS+SQLCODE, user-defined,
+-- application errors, pragma mapping, nested re-raise, translation, central logger,
+-- and cleanup patterns.
---
+++ Section_08_robust_error_management/Session_048_exception_handling_philosophy/plsql_exception_handling_philosophy.sql
@@ -0,0 +1,4 @@
+SET SERVEROUTPUT ON;
+-- See detailed, commented examples covering predefined/system/user-defined exceptions,
+-- SQLCODE/SQLERRM logging, RAISE/RAISE_APPLICATION_ERROR, PRAGMA EXCEPTION_INIT,
+-- nested propagation and re-raise, and centralized logging helpers.
